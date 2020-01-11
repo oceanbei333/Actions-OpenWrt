2ca42f3ec3ad65a8c6b55b15f4efc1b5f926732f
zhhy <hyram@wudun.net>
2020-01-11 13:04:16 +0800

    add trojan

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/trojan-gfw/openwrt-trojan.git
+cp -r openwrt-trojan/openssl1.1  package/openssl1.1
+cp -r openwrt-trojan/trojan  package/trojan
